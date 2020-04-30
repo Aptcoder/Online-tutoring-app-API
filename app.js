b9ff2fddf37a045fd81be7eec201a5299615b8f2
Aptcoder <omilosamuel@gmail.com>
2020-04-30 02:12:34 +0100

    add tutor sign up functionality

--- app.js
+++ app.js
@@ -2,22 +2,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Category = require('./models/category')
 
-
-
+//my modules
+const studenRouter = require('./routes/userRoutes/studentRoutes')
+const tutorRouter = require('./routes/userRoutes/tutorRoutes')
 mongoose.Promise = global.Promise
 
 
 var url = 'mongodb://localhost:27017/OnlineTutoring'
-mongoose.connect(url,(err) => {
-    console.log("Error connecting to database :" + err)
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err) => {
+    if(err){
+        console.log("Error connecting to database :" + err)
+    }
 })
 var app = express();
+app.use(bodyParser.json());
+
+var cat = new Category({
+    name : "primary",
+    full_name : "primary school"
+})
+
+
+cat.save().then((res) => {
+    console.log(res)
+}).catch(err => {
+    console.log("error : " + err)
+})
 
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
+
+app.use('/user/student',studenRouter)
+
+app.use('/user/tutors',tutorRouter)
 
 //start(create) server
 app.listen(3000,() => {
